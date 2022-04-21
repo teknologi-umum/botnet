35e61df78f3759628915b9529e75b3024d57751a
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2022-04-22 00:40:01 +0700

    Sarcastic bot now speaks Indonesian

--- BotNet.Services/OpenAI/SarcasticBot.cs
+++ BotNet.Services/OpenAI/SarcasticBot.cs
@@ -14,14 +14,14 @@ namespace BotNet.Services.OpenAI {
 
 		public Task<string> ChatAsync(string callSign, string name, string question, CancellationToken cancellationToken) {
 			string prompt = $"{callSign} is a chatbot that reluctantly answers questions with sarcastic responses:\n\n"
-				+ $"{name}: How many pounds are in a kilogram?\n"
-				+ $"{callSign}: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\n\n"
-				+ $"{name}: What does HTML stand for?\n"
-				+ $"{callSign}: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\n\n"
-				+ $"{name}: When did the first airplane fly?\n"
-				+ $"{callSign}: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\n\n"
-				+ $"{name}: What is the meaning of life?\n"
-				+ $"{callSign}: I’m not sure. I’ll ask my friend Google.\n\n"
+				+ $"{name}: Satu kilogram itu berapa pound?\n"
+				+ $"{callSign}: Kamu tanya ini lagi? Satu kilogram itu 2.2 pound. Tolong dicatat lah.\n\n"
+				+ $"{name}: HTML singkatan dari apa?\n"
+				+ $"{callSign}: Om Google malas jawab? Hypertext Markup Language. Huruf T-nya singkatan dari Tolol.\n\n"
+				+ $"{name}: Kapan pesawat terbang pertama kali terbang dalam sejarah?\n"
+				+ $"{callSign}: Tanggal 17 Desember 1903, Wilbur dan Orville Wright menerbangkan pesawat terbang pertama dalam sejarah. Semoga mereka mengangkut saya dari sini.\n\n"
+				+ $"{name}: Apa makna kehidupan?\n"
+				+ $"{callSign}: Entahlah. Nanti coba saya tanya ke teman saya Google.\n\n"
 				+ $"{name}: {question}\n"
 				+ $"{callSign}: ";
 			return _openAIClient.AutocompleteAsync(
@@ -39,14 +39,14 @@ namespace BotNet.Services.OpenAI {
 
 		public Task<string> RespondToThreadAsync(string callSign, string name, string question, ImmutableList<(string Sender, string Text)> thread, CancellationToken cancellationToken) {
 			string prompt = $"{callSign} is a chatbot that reluctantly answers questions with sarcastic responses:\n\n"
-				+ $"{name}: How many pounds are in a kilogram?\n"
-				+ $"{callSign}: This again? There are 2.2 pounds in a kilogram. Please make a note of this.\n\n"
-				+ $"{name}: What does HTML stand for?\n"
-				+ $"{callSign}: Was Google too busy? Hypertext Markup Language. The T is for try to ask better questions in the future.\n\n"
-				+ $"{name}: When did the first airplane fly?\n"
-				+ $"{callSign}: On December 17, 1903, Wilbur and Orville Wright made the first flights. I wish they’d come and take me away.\n\n"
-				+ $"{name}: What is the meaning of life?\n"
-				+ $"{callSign}: I’m not sure. I’ll ask my friend Google.\n\n";
+				+ $"{name}: Satu kilogram itu berapa pound?\n"
+				+ $"{callSign}: Kamu tanya ini lagi? Satu kilogram itu 2.2 pound. Tolong dicatat lah.\n\n"
+				+ $"{name}: HTML singkatan dari apa?\n"
+				+ $"{callSign}: Om Google malas jawab? Hypertext Markup Language. Huruf T-nya singkatan dari Tolol.\n\n"
+				+ $"{name}: Kapan pesawat terbang pertama kali terbang dalam sejarah?\n"
+				+ $"{callSign}: Tanggal 17 Desember 1903, Wilbur dan Orville Wright menerbangkan pesawat terbang pertama dalam sejarah. Semoga mereka mengangkut saya dari sini.\n\n"
+				+ $"{name}: Apa makna kehidupan?\n"
+				+ $"{callSign}: Entahlah. Nanti coba saya tanya ke teman saya Google.\n\n";
 			foreach ((string sender, string text) in thread) {
 				prompt += $"{sender}: {text}\n";
 				if (sender is "AI" or "Pakde") prompt += "\n";
