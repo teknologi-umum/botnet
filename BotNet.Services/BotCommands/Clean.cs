bd4b02dfe5ad2b20344cd7dbc4f5b685124633bd
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2022-06-07 12:25:16 +0700

    Add instagram link sanitizer

--- BotNet.Services/BotCommands/Clean.cs
+++ BotNet.Services/BotCommands/Clean.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using BotNet.Services.Instagram;
 using BotNet.Services.Tiktok;
 using BotNet.Services.Twitter;
 using Microsoft.Extensions.DependencyInjection;
@@ -38,6 +39,13 @@ namespace BotNet.Services.BotCommands {
 							text: $"Link yang sudah dibersihkan: {sanitizedLinkUri.OriginalString}",
 							replyToMessageId: message.MessageId,
 							cancellationToken: cancellationToken);
+					} else if (InstagramLinkSanitizer.FindTrackedInstagramLink(commandArgument) is Uri trackedInstagramUri) {
+						Uri sanitizedLinkUri = InstagramLinkSanitizer.Sanitize(trackedInstagramUri);
+						await botClient.SendTextMessageAsync(
+							chatId: message.Chat.Id,
+							text: $"Link yang sudah dibersihkan: {sanitizedLinkUri.OriginalString}",
+							replyToMessageId: message.MessageId,
+							cancellationToken: cancellationToken);
 					} else {
 						await botClient.SendTextMessageAsync(
 							chatId: message.Chat.Id,
@@ -65,6 +73,13 @@ namespace BotNet.Services.BotCommands {
 						}
 					} else if (TwitterLinkSanitizer.FindTrackedTwitterLink(commandArgument) is Uri trackedTwitterUri) {
 						Uri sanitizedLinkUri = TwitterLinkSanitizer.Sanitize(trackedTwitterUri);
+						await botClient.SendTextMessageAsync(
+							chatId: message.Chat.Id,
+							text: $"Link yang sudah dibersihkan: {sanitizedLinkUri.OriginalString}",
+							replyToMessageId: message.ReplyToMessage.MessageId,
+							cancellationToken: cancellationToken);
+					} else if (InstagramLinkSanitizer.FindTrackedInstagramLink(commandArgument) is Uri trackedInstagramUri) {
+						Uri sanitizedLinkUri = InstagramLinkSanitizer.Sanitize(trackedInstagramUri);
 						await botClient.SendTextMessageAsync(
 							chatId: message.Chat.Id,
 							text: $"Link yang sudah dibersihkan: {sanitizedLinkUri.OriginalString}",
---
+++ BotNet.Services/Instagram/InstagramLinkSanitizer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace BotNet.Services.Instagram {
+	public class InstagramLinkSanitizer {
+		public static Uri Sanitize(Uri link) {
+			string sanitizedUri = link.GetLeftPart(UriPartial.Path);
+			return new Uri(sanitizedUri);
+		}
+
+		public static Uri? FindTrackedInstagramLink(string message) {
+			return Regex.Matches(message, "^https://www.instagram.com/p/[0-9a-zA-Z]{8,16}/\\?")
+				.Select(match => new Uri(match.Value))
+				.FirstOrDefault();
+		}
+	}
+}
