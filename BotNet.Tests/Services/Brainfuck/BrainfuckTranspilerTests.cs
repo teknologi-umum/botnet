ce3c6c74985d98759df62519f34c31fc28a474c4
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2022-08-03 08:39:09 +0700

    Fix fuck tests

--- BotNet.Tests/Services/Brainfuck/BrainfuckTranspilerTests.cs
+++ BotNet.Tests/Services/Brainfuck/BrainfuckTranspilerTests.cs
@@ -13,12 +13,13 @@ namespace BotNet.Tests.Services.Brainfuck {
 		}
 
 		[Fact]
-		public void TranspilerStringShouldFuckToOriginalString() {
+		public void TranspiledStringShouldFuckToOriginalString() {
 			BrainfuckTranspiler transpiler = new();
 			string s = "The quick brown fox 12345 jumps over the lazy dog 67890";
 			string fucked = transpiler.TranspileBrainfuck(s);
 			string unfucked = BrainfuckInterpreter.RunBrainfuck(fucked);
 			unfucked.Should().Be(s);
+			unfucked.Should().NotBe(fucked);
 		}
 	}
 }
