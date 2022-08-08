d5976f5c1ec49eb2a331b81e6b5c8dd08b6d093c
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2022-08-08 07:40:53 +0700

    Fix twitter link sanitizer

--- BotNet.Services/Twitter/TwitterLinkSanitizer.cs
+++ BotNet.Services/Twitter/TwitterLinkSanitizer.cs
@@ -10,7 +10,7 @@ namespace BotNet.Services.Twitter {
 		}
 
 		public static Uri? FindTrackedTwitterLink(string message) {
-			return Regex.Matches(message, "^https://twitter.com/[0-9a-zA-Z_]+/status/[0-9]{18,20}\\?")
+			return Regex.Matches(message, "https://twitter.com/[0-9a-zA-Z_]+/status/[0-9]{18,20}\\?")
 				.Select(match => new Uri(match.Value))
 				.FirstOrDefault();
 		}
