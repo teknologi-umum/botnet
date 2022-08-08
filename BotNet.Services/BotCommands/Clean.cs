c9b673b16dbe1434c442fc401e13ba396027d4f6
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2022-08-08 08:11:47 +0700

    Add sanitizer tests

--- BotNet.Services/BotCommands/Clean.cs
+++ BotNet.Services/BotCommands/Clean.cs
@@ -71,14 +71,14 @@ namespace BotNet.Services.BotCommands {
 								replyToMessageId: message.MessageId,
 								cancellationToken: cancellationToken);
 						}
-					} else if (TwitterLinkSanitizer.FindTrackedTwitterLink(commandArgument) is Uri trackedTwitterUri) {
+					} else if (TwitterLinkSanitizer.FindTrackedTwitterLink(repliedToMessage) is Uri trackedTwitterUri) {
 						Uri sanitizedLinkUri = TwitterLinkSanitizer.Sanitize(trackedTwitterUri);
 						await botClient.SendTextMessageAsync(
 							chatId: message.Chat.Id,
 							text: $"Link yang sudah dibersihkan: {sanitizedLinkUri.OriginalString}",
 							replyToMessageId: message.ReplyToMessage.MessageId,
 							cancellationToken: cancellationToken);
-					} else if (InstagramLinkSanitizer.FindTrackedInstagramLink(commandArgument) is Uri trackedInstagramUri) {
+					} else if (InstagramLinkSanitizer.FindTrackedInstagramLink(repliedToMessage) is Uri trackedInstagramUri) {
 						Uri sanitizedLinkUri = InstagramLinkSanitizer.Sanitize(trackedInstagramUri);
 						await botClient.SendTextMessageAsync(
 							chatId: message.Chat.Id,
---
+++ BotNet.Tests/Services/LinkSanitizers/RegexTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using BotNet.Services.Tiktok;
+using BotNet.Services.Twitter;
+using FluentAssertions;
+using Xunit;
+
+namespace BotNet.Tests.Services.LinkSanitizers {
+	public class RegexTests {
+		[Theory]
+		[InlineData("https://twitter.com/ShowwcaseHQ/status/1556259601829576707?t=S6GuFx37mAXOLI2wdusfXg&s=19", "https://twitter.com/ShowwcaseHQ/status/1556259601829576707")]
+		[InlineData("WKWKWK alisnya Kevin https://twitter.com/ShowwcaseHQ/status/1556259601829576707?t=S6GuFx37mAXOLI2wdusfXg&s=19 😂", "https://twitter.com/ShowwcaseHQ/status/1556259601829576707")]
+		[InlineData("https://twitter.com/ShowwcaseHQ/status/1556259601829576707", null)]
+		public void CanSanitizeTwitterLinks(string url, string? cleaned) {
+			if (TwitterLinkSanitizer.FindTrackedTwitterLink(url) is Uri trackedUrl) {
+				Uri cleanedUrl = TwitterLinkSanitizer.Sanitize(trackedUrl);
+				cleanedUrl.OriginalString.Should().Be(cleaned);
+			} else {
+				cleaned.Should().BeNull();
+			}
+		}
+
+		[Theory]
+		[InlineData("https://vt.tiktok.com/ZSR6XLMHh/?k=1", "https://vt.tiktok.com/ZSR6XLMHh/")]
+		[InlineData("anjayyyyhttps://vt.tiktok.com/ZSR6XLMHh/?k=1", "https://vt.tiktok.com/ZSR6XLMHh/")]
+		[InlineData("https://vt.tiktok.com/ZSR6XLMHh/", "https://vt.tiktok.com/ZSR6XLMHh/")]
+		[InlineData("https://vt.tiktok.com/ZSR6XLMHh", "https://vt.tiktok.com/ZSR6XLMHh")]
+		[InlineData("https://twitter.com/ShowwcaseHQ/status/1556259601829576707?t=S6GuFx37mAXOLI2wdusfXg&s=19", null)]
+		public void CanDetectTrackedTiktokLinks(string url, string? trackedUrl) {
+			if (TiktokLinkSanitizer.FindShortenedTiktokLink(url) is Uri shortenedTiktokLink) {
+				shortenedTiktokLink.OriginalString.Should().Be(trackedUrl);
+			} else {
+				trackedUrl.Should().BeNull();
+			}
+		}
+	}
+}
