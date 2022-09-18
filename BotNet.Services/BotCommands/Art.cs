4a1d2f08d0a54d0b5041074ceb5f6f3831bba430
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2022-09-18 15:27:06 +0700

    Generate art in asynchronous task

--- BotNet.Services/BotCommands/Art.cs
+++ BotNet.Services/BotCommands/Art.cs
@@ -15,65 +15,65 @@ using Telegram.Bot.Types.InputFiles;
 
 namespace BotNet.Services.BotCommands {
 	public static class Art {
-		private static readonly RateLimiter RATE_LIMITER = RateLimiter.PerChat(2, TimeSpan.FromMinutes(2));
+		private static readonly RateLimiter RANDOM_ART_RATE_LIMITER = RateLimiter.PerChat(2, TimeSpan.FromMinutes(2));
+		private static readonly RateLimiter GENERATED_ART_RATE_LIMITER = RateLimiter.PerUserPerChat(1, TimeSpan.FromMinutes(5));
 		private static readonly HashSet<int> HANDLED_MESSAGE_IDS = new();
 		public static async Task GetRandomArtAsync(ITelegramBotClient botClient, IServiceProvider serviceProvider, Message message, CancellationToken cancellationToken) {
 			if (message.Entities?.FirstOrDefault() is { Type: MessageEntityType.BotCommand, Offset: 0, Length: int commandLength }
 				&& message.Text![commandLength..].Trim() is string commandArgument) {
 				if (commandArgument.Length > 0) {
-					if (HANDLED_MESSAGE_IDS.Contains(message.MessageId)) return;
-					HANDLED_MESSAGE_IDS.Add(message.MessageId);
 					try {
-						RATE_LIMITER.ValidateActionRate(message.Chat.Id, message.From!.Id);
+						GENERATED_ART_RATE_LIMITER.ValidateActionRate(message.Chat.Id, message.From!.Id);
 
 						DateTime started = DateTime.Now;
 						Message responseMessage = await botClient.SendTextMessageAsync(
 							chatId: message.Chat.Id,
 							text: "Generating art...",
 							parseMode: ParseMode.Html,
-							replyToMessageId: message.MessageId,
-							cancellationToken: cancellationToken);
-						try {
-							List<byte[]> images = await serviceProvider.GetRequiredService<CraiyonClient>().GenerateImagesAsync(commandArgument, cancellationToken);
-							DateTime finished = DateTime.Now;
-							TimeSpan duration = finished - started;
+							replyToMessageId: message.MessageId);
 
-							List<(Stream Stream, int Id)> imageStreams = new();
-							for (int i = 0; i < images.Count; i++) {
-								byte[] image = images[i];
-								Stream imageStream = new MemoryStream(image);
-								imageStreams.Add((imageStream, i + 1));
-							}
+						Task.Run(async () => {
 							try {
+								List<byte[]> images = await serviceProvider.GetRequiredService<CraiyonClient>().GenerateImagesAsync(commandArgument, CancellationToken.None);
+								DateTime finished = DateTime.Now;
+								TimeSpan duration = finished - started;
+
+								List<(Stream Stream, int Id)> imageStreams = new();
+								for (int i = 0; i < images.Count; i++) {
+									byte[] image = images[i];
+									Stream imageStream = new MemoryStream(image);
+									imageStreams.Add((imageStream, i + 1));
+								}
 								try {
-									await botClient.DeleteMessageAsync(
+									try {
+										await botClient.DeleteMessageAsync(
+											chatId: message.Chat.Id,
+											messageId: responseMessage.MessageId);
+									} catch {
+										// Resume when message has already been deleted
+									}
+									await botClient.SendMediaGroupAsync(
 										chatId: message.Chat.Id,
-										messageId: responseMessage.MessageId);
-								} catch {
-									// Resume when message has already been deleted
+										media: imageStreams.Select(imageStream => (IAlbumInputMedia)new InputMediaPhoto(new InputMedia(imageStream.Stream, $"IMG{imageStream.Id}.png"))),
+										replyToMessageId: message.MessageId);
+								} finally {
+									foreach ((Stream imageStream, _) in imageStreams) {
+										imageStream.Dispose();
+									}
 								}
-								await botClient.SendMediaGroupAsync(
+							} catch {
+								await botClient.EditMessageTextAsync(
 									chatId: message.Chat.Id,
-									media: imageStreams.Select(imageStream => (IAlbumInputMedia)new InputMediaPhoto(new InputMedia(imageStream.Stream, $"IMG{imageStream.Id}.png"))),
-									replyToMessageId: message.MessageId,
-									cancellationToken: cancellationToken);
-							} finally {
-								foreach ((Stream imageStream, _) in imageStreams) {
-									imageStream.Dispose();
-								}
+									messageId: responseMessage.MessageId,
+									text: "<code>Could not generate art</code>",
+									parseMode: ParseMode.Html
+								);
 							}
-						} catch {
-							await botClient.EditMessageTextAsync(
-								chatId: message.Chat.Id,
-								messageId: responseMessage.MessageId,
-								text: "<code>Could not generate art</code>",
-								parseMode: ParseMode.Html
-							);
-						}
+						});
 					} catch (RateLimitExceededException exc) when (exc is { Cooldown: var cooldown }) {
 						await botClient.SendTextMessageAsync(
 							chatId: message.Chat.Id,
-							text: $"Saya belum selesai melukis. Coba lagi {cooldown}.",
+							text: $"Anda belum mendapat giliran. Coba lagi {cooldown}.",
 							parseMode: ParseMode.Html,
 							replyToMessageId: message.MessageId,
 							cancellationToken: cancellationToken);
@@ -81,7 +81,7 @@ namespace BotNet.Services.BotCommands {
 				}
 			} else {
 				try {
-					RATE_LIMITER.ValidateActionRate(message.Chat.Id, message.From!.Id);
+					RANDOM_ART_RATE_LIMITER.ValidateActionRate(message.Chat.Id, message.From!.Id);
 
 					byte[] image = await serviceProvider.GetRequiredService<ThisArtworkDoesNotExist>().GetRandomArtworkAsync(cancellationToken);
 					using MemoryStream imageStream = new(image);
