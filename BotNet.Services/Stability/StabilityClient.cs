16998092737e09c347196f09d30a356239232c89
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2022-12-02 18:55:12 +0700

    Crop & resize prompt image before sending

--- BotNet.Services/Stability/StabilityClient.cs
+++ BotNet.Services/Stability/StabilityClient.cs
@@ -4,7 +4,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using Google.Protobuf;
 using Grpc.Core;
+using Grpc.Core.Logging;
 using Grpc.Net.Client;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using SkiaSharp;
 
@@ -13,14 +15,17 @@ namespace BotNet.Services.Stability {
 		private readonly GrpcChannel _grpcChannel;
 		private readonly GenerationService.GenerationServiceClient _generationServiceClient;
 		private readonly string _apiKey;
+		private readonly ILogger<StabilityClient> _logger;
 		private bool _disposedValue;
 
 		public StabilityClient(
-			IOptions<StabilityOptions> optionsAccessor
+			IOptions<StabilityOptions> optionsAccessor,
+			ILogger<StabilityClient> logger
 		) {
 			_grpcChannel = GrpcChannel.ForAddress("https://grpc.stability.ai/");
 			_generationServiceClient = new GenerationService.GenerationServiceClient(_grpcChannel);
 			_apiKey = optionsAccessor.Value.ApiKey!;
+			_logger = logger;
 		}
 
 		public async Task<byte[]> GenerateImageAsync(string promptText, CancellationToken cancellationToken) {
@@ -80,26 +85,45 @@ namespace BotNet.Services.Stability {
 				await _grpcChannel.ConnectAsync(cancellationToken);
 			}
 
-			// Convert to png
 			using SKBitmap bitmap = SKBitmap.Decode(imagePrompt);
-			using SKSurface surface = SKSurface.Create(new SKImageInfo(bitmap.Width, bitmap.Height));
+
+			// Crop to square and resize to nearest multiple of 64
+			int viewportSize = Math.Min(bitmap.Width, bitmap.Height);
+			int promptSize = viewportSize % 64 > 0 ? (viewportSize / 64 + 1) * 64 : viewportSize;
+			using SKSurface surface = SKSurface.Create(new SKImageInfo(promptSize, promptSize));
 			using SKCanvas canvas = surface.Canvas;
 			canvas.DrawBitmap(
 				bitmap: bitmap,
-				source: SKRect.Create(bitmap.Width, bitmap.Height),
-				dest: SKRect.Create(bitmap.Width, bitmap.Height));
+				source: bitmap.Width > bitmap.Height
+					? SKRect.Create(
+						x: (bitmap.Width - viewportSize) / 2f,
+						y: 0f,
+						width: viewportSize,
+						height: viewportSize
+					)
+					: SKRect.Create(
+						x: 0f,
+						y: (bitmap.Height - viewportSize) / 2f,
+						width: viewportSize,
+						height: viewportSize
+					),
+				dest: SKRect.Create(promptSize, promptSize)
+			);
 			canvas.Flush();
+
+			// Convert to PNG
 			using SKImage image = surface.Snapshot();
 			using SKData data = image.Encode(SKEncodedImageFormat.Png, 80);
 			using MemoryStream convertedImageStream = new();
 			data.SaveTo(convertedImageStream);
 			imagePrompt = convertedImageStream.ToArray();
 
-			AsyncServerStreamingCall<Answer> streamingCall = _generationServiceClient.Generate(
-				request: new Request {
-					EngineId = "stable-diffusion-v1",
-					RequestId = Guid.NewGuid().ToString(),
-					Prompt = {
+			try {
+				AsyncServerStreamingCall<Answer> streamingCall = _generationServiceClient.Generate(
+					request: new Request {
+						EngineId = "stable-diffusion-v1",
+						RequestId = Guid.NewGuid().ToString(),
+						Prompt = {
 					new Prompt {
 						Text = textPrompt
 					},
@@ -113,16 +137,16 @@ namespace BotNet.Services.Stability {
 							Init = true
 						}
 					}
-					},
-					Image = new ImageParameters {
-						Width = 512,
-						Height = 512,
-						Steps = 10,
-						Samples = 1,
-						Transform = new TransformType {
-							Diffusion = DiffusionSampler.SamplerKLms
 						},
-						Parameters = {
+						Image = new ImageParameters {
+							Width = 512,
+							Height = 512,
+							Steps = 10,
+							Samples = 1,
+							Transform = new TransformType {
+								Diffusion = DiffusionSampler.SamplerKLms
+							},
+							Parameters = {
 						new StepParameter {
 							ScaledStep = 0,
 							Sampler = new SamplerParameters {
@@ -133,24 +157,28 @@ namespace BotNet.Services.Stability {
 								End = 0.01f
 							}
 						}
-						},
-						Seed = {
+							},
+							Seed = {
 						(uint)Random.Shared.Next()
+							}
+						}
+					},
+					headers: new Metadata {
+				{ "Authorization", $"Bearer {_apiKey}" }
+					},
+					cancellationToken: cancellationToken
+				);
+
+				await foreach (Answer answer in streamingCall.ResponseStream.ReadAllAsync(cancellationToken)) {
+					foreach (Artifact artifact in answer.Artifacts) {
+						if (artifact.Type == ArtifactType.ArtifactImage) {
+							return artifact.Binary.ToByteArray();
 						}
 					}
-				},
-				headers: new Metadata {
-				{ "Authorization", $"Bearer {_apiKey}" }
-				},
-				cancellationToken: cancellationToken
-			);
-
-			await foreach (Answer answer in streamingCall.ResponseStream.ReadAllAsync(cancellationToken)) {
-				foreach (Artifact artifact in answer.Artifacts) {
-					if (artifact.Type == ArtifactType.ArtifactImage) {
-						return artifact.Binary.ToByteArray();
-					}
 				}
+			} catch (Exception exc) {
+				_logger.LogError(exc, "Unable to generate image");
+				throw;
 			}
 
 			throw new InvalidOperationException("Unable to generate image");
