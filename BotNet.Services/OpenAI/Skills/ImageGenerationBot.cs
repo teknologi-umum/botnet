a16d81e747fd68a82ef2bc717207005ef9dbeb65
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2023-12-03 19:13:43 +0700

    Limit image generation concurrency

--- BotNet.Services/OpenAI/Skills/ImageGenerationBot.cs
+++ BotNet.Services/OpenAI/Skills/ImageGenerationBot.cs
@@ -7,14 +7,23 @@ namespace BotNet.Services.OpenAI.Skills {
 		OpenAIClient openAIClient
 	) {
 		private readonly OpenAIClient _openAIClient = openAIClient;
+		private static readonly SemaphoreSlim SEMAPHORE = new(1, 1);
 
-		public Task<Uri> GenerateImageAsync(
+		public async Task<Uri> GenerateImageAsync(
 			string prompt,
 			CancellationToken cancellationToken
-		) => _openAIClient.GenerateImageAsync(
-			model: "dall-e-3",
-			prompt: prompt,
-			cancellationToken: cancellationToken
-		);
+		) {
+			// dall-e-3 endpoint does not allow concurrent requests
+			await SEMAPHORE.WaitAsync(cancellationToken);
+			try {
+				return await _openAIClient.GenerateImageAsync(
+					model: "dall-e-3",
+					prompt: prompt,
+					cancellationToken: cancellationToken
+				);
+			} finally {
+				SEMAPHORE.Release();
+			}
+		}
 	}
 }
