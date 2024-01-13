395b3c4b9af64fed1cccc0db107fda958003f317
Bagas Wastu <67826350+bgwastu@users.noreply.github.com>
2024-01-13 12:47:39 +0700

    feat: add url cleaner feature

---
+++ BotNet.Services/CleanUrl/UrlCleaner.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace BotNet.Services.UrlCleaner {
+  public partial class UrlCleaner {
+    /// <summary>
+    /// Cleans the specified URL by removing query parameters based on predefined rules.
+    /// </summary>
+    /// <param name="url">The URL to be cleaned.</param>
+    /// <returns>A cleaned URI.</returns>
+    public static Uri Clean(Uri url) {
+      foreach (Rule rule in RuleData.Rules) {
+        if (rule.Match.IsMatch(url.ToString())) {
+          foreach (string r in rule.Rules) {
+            url = new Uri(Regex.Replace(url.ToString(), $"[&?]({r})=[^&]*", ""));
+          }
+        }
+      }
+
+      // Remove trailing '?' or '&' if present
+      string cleanedUrl = url.ToString().TrimEnd('?', '&').TrimEnd('/');
+
+      return new Uri(cleanedUrl);
+    }
+  }
+}
