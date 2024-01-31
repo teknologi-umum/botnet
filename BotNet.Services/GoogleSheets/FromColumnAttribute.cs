f2a4779e97d954a65fbdb637d33f8749a900826e
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2024-01-31 11:55:35 +0700

    Scaffold VPS data source

---
+++ BotNet.Services/GoogleSheets/FromColumnAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BotNet.Services.GoogleSheets {
+	[AttributeUsage(validOn: AttributeTargets.Property, AllowMultiple = false)]
+	public sealed class FromColumnAttribute : Attribute {
+		public string Column { get; }
+
+		public FromColumnAttribute(string column) {
+			Column = column;
+		}
+	}
+}
---
+++ BotNet.Services/VPS/VPSBenchmark.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using BotNet.Services.GoogleSheets;
+
+namespace BotNet.Services.VPS {
+	public sealed record VPSBenchmark(
+		[property: FromColumn("A")] string Provider,
+		[property: FromColumn("B")] string Location,
+		[property: FromColumn("C")] DateOnly BenchmarkDate,
+		[property: FromColumn("E")] string? VerdictCons,
+		[property: FromColumn("F")] decimal IdrMo,
+		[property: FromColumn("G")] int Core,
+		[property: FromColumn("H")] int SsdGb,
+		[property: FromColumn("I")] int RamMb,
+		[property: FromColumn("J")] int IoMbs,
+		[property: FromColumn("K")] double? ToCacheFlyMbs,
+		[property: FromColumn("L")] double? ToHkCnMbs,
+		[property: FromColumn("M")] double? ToLinodeJpMbs,
+		[property: FromColumn("N")] double? ToLinodeSgMbs,
+		[property: FromColumn("O")] double? ToLinodeUkMbs,
+		[property: FromColumn("P")] double? ToLinodeCaMbs,
+		[property: FromColumn("R")] double? BzipSec,
+		[property: FromColumn("S")] double? DlMbs,
+		[property: FromColumn("T")] double? AvgMbs
+	);
+}
