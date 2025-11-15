7446b97994c00acd9948b9bba6b1d0935ac27d1f
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2025-11-15 23:38:17 +0700

    Add rate limiting to /no command
    - Home group chat: 5 invocations per 2 minutes per user
    - Other group chat: 2 invocations per 2 minutes per user
    - Private chat: 20 invocations per 2 minutes per user
    - VIP users: No rate limit
    - Rate limit message: 'Kebanyakan say no. Coba lagi {cooldown}.'

--- BotNet.CommandHandlers/No/NoCommandHandler.cs
+++ BotNet.CommandHandlers/No/NoCommandHandler.cs
@@ -1,5 +1,8 @@
+using BotNet.Commands.ChatAggregate;
 using BotNet.Commands.No;
+using BotNet.Commands.SenderAggregate;
 using BotNet.Services.NoAsAService;
+using BotNet.Services.RateLimit;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
@@ -9,7 +12,49 @@ namespace BotNet.CommandHandlers.No {
 		ITelegramBotClient telegramBotClient,
 		NoAsAServiceClient noAsAServiceClient
 	) : ICommandHandler<NoCommand> {
+		private static readonly RateLimiter HomeGroupChatRateLimiter = RateLimiter.PerUserPerChat(5, TimeSpan.FromMinutes(2));
+		private static readonly RateLimiter GroupChatRateLimiter = RateLimiter.PerUserPerChat(2, TimeSpan.FromMinutes(2));
+		private static readonly RateLimiter PrivateChatRateLimiter = RateLimiter.PerUserPerChat(20, TimeSpan.FromMinutes(2));
+
 		public async Task Handle(NoCommand command, CancellationToken cancellationToken) {
+			// No rate limit for VIP
+			if (command.Command.Sender is not VipSender) {
+				try {
+					// Apply different rate limits based on chat type
+					switch (command.Command.Chat) {
+						case HomeGroupChat:
+							HomeGroupChatRateLimiter.ValidateActionRate(
+								chatId: command.Command.Chat.Id,
+								userId: command.Command.Sender.Id
+							);
+							break;
+						case GroupChat:
+							GroupChatRateLimiter.ValidateActionRate(
+								chatId: command.Command.Chat.Id,
+								userId: command.Command.Sender.Id
+							);
+							break;
+						case PrivateChat:
+							PrivateChatRateLimiter.ValidateActionRate(
+								chatId: command.Command.Chat.Id,
+								userId: command.Command.Sender.Id
+							);
+							break;
+					}
+			} catch (RateLimitExceededException exc) {
+				await telegramBotClient.SendMessage(
+					chatId: command.Command.Chat.Id,
+					text: $"<code>Kebanyakan say no. Coba lagi {exc.Cooldown}.</code>",
+					parseMode: ParseMode.Html,
+					replyParameters: new ReplyParameters {
+							MessageId = command.Command.MessageId
+						},
+						cancellationToken: cancellationToken
+					);
+					return;
+				}
+			}
+
 			string reason = await noAsAServiceClient.GetNoReasonAsync(cancellationToken);
 
 			await telegramBotClient.SendMessage(
