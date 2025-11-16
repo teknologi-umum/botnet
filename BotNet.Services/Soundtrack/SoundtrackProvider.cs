017c65270ccf53efe48e0fa6b06d44e48251b4a5
Ronny Gunawan <3048897+ronnygunawan@users.noreply.github.com>
2025-11-16 12:26:33 +0700

    Add 4 more music sites to /soundtrack pool
    Added:
    - DevTunes FM (radio.madza.dev)
    - Nightride FM (nightride.fm)
    - Code Radio by freeCodeCamp (coderadio.freecodecamp.org)
    - FilterMusic (filtermusic.net)
    
    Total curated sites now: 9 (up from 5)

--- BotNet.Services/Soundtrack/SoundtrackProvider.cs
+++ BotNet.Services/Soundtrack/SoundtrackProvider.cs
@@ -4,15 +4,19 @@ using System.Linq;
 
 namespace BotNet.Services.Soundtrack {
 	public class SoundtrackProvider {
-		private static readonly List<SoundtrackSite> Sites = new() {
-			new("musicForProgramming()", "https://musicforprogramming.net/latest"),
-			new("Poolsuite FM", "https://poolsuite.net/"),
-			new("Lofi Cafe", "https://lofi.cafe/"),
-			new("Radio Garden", "https://radio.garden/"),
-			new("Chillhop", "https://app.chillhop.com/")
-		};
+	private static readonly List<SoundtrackSite> Sites = new() {
+		new("musicForProgramming()", "https://musicforprogramming.net/latest"),
+		new("Poolsuite FM", "https://poolsuite.net/"),
+		new("Lofi Cafe", "https://lofi.cafe/"),
+		new("Radio Garden", "https://radio.garden/"),
+		new("Chillhop", "https://app.chillhop.com/"),
+		new("DevTunes FM", "https://radio.madza.dev/"),
+		new("Nightride FM", "https://nightride.fm/"),
+		new("Code Radio", "https://coderadio.freecodecamp.org/"),
+		new("FilterMusic", "https://filtermusic.net/")
+	};
 
-		public (SoundtrackSite First, SoundtrackSite Second) GetHourlyPicks() {
+	public (SoundtrackSite First, SoundtrackSite Second) GetHourlyPicks() {
 			DateTime now = DateTime.UtcNow;
 			// Seed based on year + day + hour for hourly rotation
 			int seed = now.Year * 10000 + now.DayOfYear * 100 + now.Hour;
